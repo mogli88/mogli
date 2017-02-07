e8adec67474c87934f8c4479ba2afc8ef8a8aeed
mogli88 <julialp@gmx.de>
2017-02-07 18:47:56 +0100

    particles and more

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour {
 
-	public GameObject background;
 	public Material mat;
 	public AudioClip purr;
 	public AudioClip keyboard;
 	private AudioSource audio;
 	private Vector3 tmpMousePosition;
+	public CameraShake cameraShake;
+	public ParticleSystem p;
 
 	// Use this for initialization
 	void Start () {
@@ -17,6 +18,13 @@ public class InputHandler : MonoBehaviour {
 		audio.clip = keyboard;
 		audio.Play ();
 		tmpMousePosition = Input.mousePosition;
+
+		//screen shake
+		GameObject g = GameObject.Find ("Main Camera");
+		cameraShake = g.GetComponent<CameraShake> ();
+
+		ParticleSystem p = GetComponent<ParticleSystem>();
+		hideParticles ();
 	}
 
 	// Update is called once per frame
@@ -40,12 +48,16 @@ public class InputHandler : MonoBehaviour {
 			audio.Stop ();
 			audio.clip = keyboard;
 			audio.Play ();
+			cameraShake.mouseClicked = false;
+			hideParticles ();
+
 		}
 
 		if ((tmpMousePosition != Input.mousePosition) && (Input.GetMouseButtonDown (0))){
 			Debug.Log("Mouse moved");
 			tmpMousePosition = Input.mousePosition;
-
+			cameraShake.mouseClicked = true;
+			seeParticles();
 			mat.color = new Color32 (150, 150, 150, 255);
 			audio.Stop ();
 			audio.clip = purr;
@@ -53,6 +65,19 @@ public class InputHandler : MonoBehaviour {
 
 		}
 
+	}
+
+
+	void seeParticles()
+	{
+		var main = p.main;
+		main.startColor	= new Color (150, 150, 150, 100);
+	}
+
+	void hideParticles()
+	{
+		var main = p.main;
+		main.startColor	= new Color (150, 150, 150, 0);
 	}
 
 }
---
+++ Assets/Scripts/MouseFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour {
+
+	public float distance = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray r = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Vector3 pos = r.GetPoint (distance);
+		transform.position = pos;
+	}
+}
